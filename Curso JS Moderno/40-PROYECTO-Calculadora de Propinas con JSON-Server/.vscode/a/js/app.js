2068f1efb9c89ee70f3cc73d4b3aa867a341465f
devxyzr <develop.xyzr@gmail.com>
2022-06-15 12:51:05 -0500

    Client API progress

--- Curso JS Moderno/40-PROYECTO-Calculadora de Propinas con JSON-Server/.vscode/a/js/app.js
+++ Curso JS Moderno/40-PROYECTO-Calculadora de Propinas con JSON-Server/.vscode/a/js/app.js
@@ -98,6 +98,13 @@ function mostrarPlatillos(platillos) {
     inputCantidad.id = "producto-${platillo.id}";
     inputCantidad.classList.add("form-control");
 
+    // Funcion que detecta la cantidad y el platillos que se esta agregando
+
+    inputCantidad.onchange = function () {
+      const cantidad = parseInt(inputCantidad.value);
+      agregarPlatillo({ ...platillo, cantidad });
+    };
+
     const agregar = document.createElement("DIV");
     agregar.classList.add("col-md-2");
     agregar.appendChild(inputCantidad);
@@ -109,4 +116,96 @@ function mostrarPlatillos(platillos) {
 
     contenido.appendChild(row);
   });
+}
+
+function agregarPlatillo(producto) {
+  // Extraer le pedido actula
+  let { pedido } = cliente;
+  // Revisar que la cantidad sea mayor a 0
+
+  if (producto.cantidad > 0) {
+    // Comprueba si el elemento ya existe en el array
+    if (pedido.some((articulo) => articulo.id === producto.id)) {
+      // El articulo ya existe, tenemos que actulizar la cantidad
+      const pedidoActualizado = pedido.map((articulo) => {
+        if (articulo.id === producto.id) {
+          articulo.cantidad = producto.cantidad;
+        }
+        return articulo;
+      });
+      // Se asigna el nuevo array a cliente.pedido
+      cliente.pedido = [...pedidoActualizado];
+    } else {
+      // Si el articulo no existe , lo agregamos al array del prodcuto
+      cliente.pedido = [...pedido, producto];
+    }
+  } else {
+    // Eliminar elemesntos cuando la cantidad es 0
+    const resultado = pedido.filter((articulo) => articulo.id !== producto.id);
+    cliente.pedido = [...resultado];
+  }
+
+  // Limpiar el codigo del HTML previo
+
+  limpiarHTML();
+
+  // Mostrar el resumen
+  actualizarResumen();
+}
+
+function actualizarResumen() {
+  const contenido = document.querySelector("#resumen .contenido");
+
+  const resumen = document.createElement("DIV");
+  resumen.classList.add("col-md-6", "card", "py-5", "px-3", "shadow");
+
+  const mesa = document.createElement("P");
+  mesa.textContent = "Mesa: ";
+  mesa.classList.add("fw-bold");
+
+  const mesaSpan = document.createElement("SPAN");
+  mesaSpan.textContent = cliente.mesa;
+  mesaSpan.classList.add("fw-normal");
+
+  const hora = document.createElement("P");
+  hora.textContent = "Hora: ";
+  hora.classList.add("fw-bold");
+
+  const horaSpan = document.createElement("SPAN");
+  horaSpan.textContent = cliente.hora;
+  horaSpan.classList.add("fw-normal");
+
+  // Agrea a los elementos padre
+  mesa.appendChild(mesaSpan);
+  hora.appendChild(horaSpan);
+
+  // Titulo de la seccion
+  const heading = document.createElement("H3");
+  heading.textContent = "Platillos consumidos ";
+  heading.classList.add("my-4", "text-center");
+
+  // Iterar sobre el array de pedidos
+
+  const grupo = document.createElement("UL");
+  grupo.classList.add("list-grupo");
+
+  const { pedido } = cliente;
+  pedido.forEach((articulo) => {
+    console.log(articulo);
+  });
+
+  // Agregar al contenido
+  resumen.appendChild(mesa);
+  resumen.appendChild(hora);
+  resumen.appendChild(heading);
+
+  contenido.appendChild(resumen);
+}
+
+function limpiarHTML() {
+  const contenido = document.querySelector("#resumen .contenido");
+
+  while (contenido.firstChild) {
+    contenido.removeChild(contenido.firstChild);
+  }
 }
