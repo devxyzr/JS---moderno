b13d73a0d6ee3676d84fa11b25c0486a3e972c59
devxyzr <develop.xyzr@gmail.com>
2022-07-05 22:27:05 -0500

    Prestamos react App

--- Curso JS Moderno/54 - React App/prestamos/src/App.js
+++ Curso JS Moderno/54 - React App/prestamos/src/App.js
@@ -1,12 +1,44 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./componentes/Header";
+import Formulario from "./componentes/Formulario";
+import Mensaje from "./componentes/Mensaje";
+import Resultado from "./componentes/Resultado";
+import Spinner from "./componentes/Resultado";
 
 function App() {
+  // Definir state
+  const [cantidad, guardarCantidad] = useState(0);
+  const [plazo, guardarPlazo] = useState("");
+  const [total, guardarTotal] = useState(0);
+  const [cargando, guardarCargando] = useState(false);
+
+  let componente;
+
+  if (cargando) {
+    componente = <Spinner />;
+  } else if (total === 0) {
+    componente = <Mensaje />;
+  } else {
+    componente = <Resultado total={total} plazo={plazo} cantidad={cantidad} />;
+  }
+
   return (
     <Fragment>
-      <Header />
-      <Header />
-      <Header />
+      <Header titulo="Cotizador de Prestamos" />
+
+      <div className="container">
+        <Formulario
+          cantidad={cantidad}
+          guardarCantidad={guardarCantidad}
+          plazo={plazo}
+          guardarPlazo={guardarPlazo}
+          total={total}
+          guardarTotal={guardarTotal}
+          guardarCargando={guardarCargando}
+        />
+
+        <div className="Mensajes">{componente}</div>
+      </div>
     </Fragment>
   );
 }
--- Curso JS Moderno/54 - React App/prestamos/src/componentes/Header.js
+++ Curso JS Moderno/54 - React App/prestamos/src/componentes/Header.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { Fragment } from "react";
 
-function Header() {
-  return <h1> Hola Mundo</h1>;
-}
+const Header = ({ titulo, cantidad }) => {
+  //----------------------------------
+
+  // Si esxiste el return dendro de la funcion este espacio antes de return se puede utilizar para inyectar JS puro
+
+  //---------------------------------
+  return (
+    <Fragment>
+      <h1> {titulo}</h1>
+    </Fragment>
+  );
+};
+
+// Se puede escribir de manera implicitael codigo, pero no se le podria inyectar condigo JavaScript dentro de la funcion
+
+// const Header2 = ({ titulo }) => {
+//   <h1>{titulo} </h1>;
+// };
 
 export default Header;
